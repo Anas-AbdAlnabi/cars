700b93f9539c611403a5e68921c712da5d121d3e
Anas-AbdAlnabi <anasar_2015@outlook.com>
2021-01-31 15:11:45 +0200

    finishing lap 5

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 alert('DOLLAR WISHS YOU A SAFE TRIP');
 
-var message='';
+var message = '';
 
 var passenger = prompt('do you resrve ?');
 
-if (passenger =='yes' ){
+if (passenger == 'yes') {
     message = 'we glad to hear that';
 
-}else if ( passenger == 'no' ){
+} else if (passenger == 'no') {
     message = 'we hope you chosing our cars';
-}else {message='please answer the qustion'
+} else {
+    message = 'please answer the qustion'
 
 }
-document.write('<h2>' + message + '</h2>');
+
+var like = '';
+
+while (like != 'yes' && like != 'no') {
+    like = prompt('Do you like cars? please answer with yes or no');
+}
+var pic = prompt('how many times you want to see your car photo?')
+for (var i = 0; i < pic; i++){
+    document.write('<img src="https://www.calderstewart.co.nz/assets/GalleryImages/_resampled/CompressedImageW10/Thrifty-Car-Rental-1.jpg">')
+}
+    document.write('<h2>' + message + '</h2>');
 alert('DONT USE PHONE WHILLE DRIVING');
